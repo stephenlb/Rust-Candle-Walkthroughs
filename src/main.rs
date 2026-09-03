88d1ddc7694deb0cc3f923a2a65cfa3be329735c
Stephen Blum <blum.stephen@gmail.com>
2026-09-03 09:50:51 -0700

    @lenam8084 thank you! 🎉

--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,10 @@ struct LinearRegression {
     device: Device,
 }
 
-/*
-squeeze
-[1,2,3] -> [[1,2,3]]
-[1,2,3] -> [[1],[2],[3]]
-
-unsqeeze
-[[1,2,3]] -> [1,2,3]
-*/
-
 impl LinearRegression {
     fn new(feature: usize, device: Device) -> Result<Self> {
         // TODO fix input weights matrix input dim
-        let weights: Tensor = Tensor::randn(0f32, 1f32, (2,1,3), &device)?;
+        let weights: Tensor = Tensor::randn(0f32, 1f32, (10), &device)?;
         let bias: Tensor = Tensor::new(0f32, &device)?;
 
         Ok(LinearRegression {
@@ -30,20 +21,31 @@ impl LinearRegression {
     }
 
     fn forward(&self, x: &Tensor) -> Result<Tensor> {
-        let out = x.matmul(&self.weights.unsqueeze(0)?)?;
-        let out = out.squeeze(1)?;
-        let out = out.broadcast_add(&self.bias)?;
+        let x = x.unsqueeze(0)?;
+        println!("x: {x}");
+        println!("weights: {}",self.weights);
+        let uns = self.weights.unsqueeze(1)?;
+        println!("uns: {uns}");
+        let out = x.matmul(&uns)?;
+        //let out = out.squeeze(1)?;
+        //let out = out.broadcast_add(&self.bias)?;
         Ok(out)
+    }
+
+    fn loss(&self, predictions: &Tensor, targets: &Tensor) -> Result<f32> {
+        let loss = predictions.sub(targets)?;
+        let loss = loss.sqr()?.mean_all()?;
+        let output: f32 = loss.to_scalar()?;
+        Ok(output)
     }
 }
 
 fn main() -> Result<()> {
-    println!("{:?}", -1);
     let device: Device = Device::metal_if_available(0)?;
     let model: LinearRegression = LinearRegression::new(10, device.clone())?;
-    let x: Tensor = Tensor::randn(0f32, 1f32, (10, 1), &device)?;
-    //model.forward(&x)?;
-    //println!("working");
+    let x: Tensor = Tensor::randn(0f32, 1f32, (10), &device)?;
+    let predictions = model.forward(&x)?;
+    //println!("{predictions}");
     Ok(())
 }
 
