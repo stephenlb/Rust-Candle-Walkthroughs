3adfe0dfcd3c8d3170fb458c48cda8e7ddc0c72b
Stephen Blum <blum.stephen@gmail.com>
2026-09-12 10:52:49 -0700

    nearly finished k-means.

--- src/main.rs
+++ src/main.rs
@@ -48,9 +48,9 @@ fn load_dataset(file: &str, device: &Device) -> Result<Tensor> {
     Ok(out)
 }
 
-fn k_means(data: &Tensor, clusters: usize, device: &Device) -> Result<()> {
+fn k_means(data: &Tensor, clusters: usize, max_iterations: usize, device: &Device) -> Result<(Tensor, Tensor)> {
     let (items, _) = data.dims2()?;
-    let mut rng = rand::rng();//.random_range(1..=100);
+    let mut rng = rand::rng();
     let mut indicies: Vec<_> = (0..items).collect();
     indicies.shuffle(&mut rng);
     let centeroid_idx: Vec<_> = indicies[..clusters]
@@ -58,16 +58,72 @@ fn k_means(data: &Tensor, clusters: usize, device: &Device) -> Result<()> {
         .copied()
         .map(|x| x as i64 )
         .collect();
-    Ok(())
+    let centroid_idx_tensor = Tensor::from_slice(
+        centeroid_idx.as_slice(), 
+        (clusters,),
+        device,
+    )?;
+
+    let mut centers = data.index_select(&centroid_idx_tensor, 0)?;
+    let mut cluster_assignments = Tensor::zeros(
+        (items,),
+        DType::U32,
+        device,
+    )?;
+
+    for _ in 0..max_iterations {
+        let dist: Tensor = cdist(data, &centers)?;
+        cluster_assignments = dist.argmin(D::Minus1)?;
+        let mut centers_vec = vec![];
+        //let mut centers_vec: Vec<f32> = Vec::new();
+
+        for c in 0..clusters {
+            let mut indicies: Vec<u32> = vec![];
+            cluster_assignments
+                .to_vec1::<u32>()?
+                .iter()
+                .enumerate()
+                .for_each(|(j, x)| {
+                    if *x == c as u32 {
+                        indicies.push(j as u32);
+                    }
+                });
+            let indicies = Tensor::from_slice(
+                indicies.as_slice(),
+                (indicies.len(),),
+                device,
+            )?;
+            let cluster_data = data.index_select(&indicies, 0)?;
+            let average = cluster_data.mean(0)?;
+            centers_vec.push(average);
+
+        }
+        centers = Tensor::stack(centers_vec.as_slice(), 0)?;
+    }
+
+    Ok((centers, cluster_assignments))
 }
 
 fn main() -> Result<()> {
     let file: &str = "data/iris.csv";
-    let device: Device = Device::metal_if_available(0)?;
+    let device: Device = Device::Cpu;
     let data: Tensor = load_dataset(
         file,
         &device,
     )?;
+
+    let (centers, cluster_assignments) =
+        k_means(
+            &data,
+            5 as usize,
+            20 as usize,
+            &device,
+        )?;
+
+    println!("{}", centers);
+    println!("{}", cluster_assignments);
+
+
     println!("data: {data}");
     Ok(())
 }
